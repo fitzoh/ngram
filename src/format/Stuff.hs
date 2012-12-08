d657b6b9f9663a51315b8c3edbc1cdc1fd902978
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2012-12-07 22:04:02 -0500

    remove unused format folder

--- src/format/Stuff.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-import Data.Char
-import Format
-import System.IO
-main::IO()
-main = do
-        fn <- getLine
-        contents <- readFile "txt/got.txt"
-        putStr $ formatString contents
